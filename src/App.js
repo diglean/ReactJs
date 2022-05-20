307ef4bb131485189eb37042a4e2cd132745c536
Diego Leandro <diegoleandro2002@hotmail.com>
2022-05-20 20:32:12 -0300

    Props and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-export default function App(){
+import React from 'react';
+
+const Bemvindo = (props) => {
+    return(
+        <div>
+            <h2>Bem vindo(a), {props.nome}</h2>
+            <h3>Tenho {props.idade} anos</h3>
+        </div>
+    );
+}
+
+function App(){
   return(
     <div>
-      <h1>Bem vindo ao sistema</h1>
-      <h2>@diglean =)</h2>
+      Olá Mundo!
+      <Bemvindo nome="Diego" idade="20"/>
+      <Bemvindo nome="Leandro" idade="21"/>
+      <h1>Diglean</h1>
     </div>
-  )
+  );
 }
+
+export default App;
