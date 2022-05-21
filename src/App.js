eebd122d57c4d210f20565d74279054e39629d18
Diego Leandro <diegoleandro2002@hotmail.com>
2022-05-21 11:01:56 -0300

    Lists

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
 import React, { Component} from 'react';
 //import Membro from './components/Membro';
+import Feed from './components/feed'
 
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = {
-        status: false
+        feed: [
+          {id: 1, username: 'Diego', curtidas:10, comentarios:2},
+          {id: 2, username: 'Leandro', curtidas:120, comentarios:22},
+          {id: 3, username: 'Silva', curtidas:102, comentarios:32},
+          {id: 4, username: 'Henrique', curtidas:1, comentarios:0}
+        ]
     };
-    this.sair = this.sair.bind(this);
-    this.entrar = this.entrar.bind(this);
-  }
 
-  sair(){
-      this.setState({status: false});
-  }
-
-  entrar(){
-      this.setState({status: true});
   }
 
     render(){
         return(
             <div>
-                {this.state.status ? 
-                <div>
-                    <h2>Bem vindo ao sistema</h2>
-                    <button onClick={this.sair}>Sair do sistema</button>
-                </div> :
-                <div>
-                  <h2>Olá visitante, faça o login</h2>
-                  <button onClick={this.entrar}>Entrar no sistema</button>
-                </div>
-                } 
+
+                {this.state.feed.map((item)=>{
+                  return(
+                    <Feed id={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
+                  );
+                })}
+
             </div>
             
 
