368dd1cfb888205ded9424e8d59c0715b8766360
Diego Leandro <diegoleandro2002@hotmail.com>
2022-05-21 09:19:32 -0300

    Events

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React, { Component} from 'react';
+import Membro from './components/Membro';
 
 class App extends Component{
 
-  constructor(props){
-    super(props);
-    this.state = { 
-      Hora: '00:00:00'
-    };
-  }
-
-  componentDidMount(){
-
-      setInterval(() => {
-        this.setState({Hora: new Date().toLocaleTimeString()})
-      }, 1000);
-  }
-
-  componentDidUpdate(){
-      console.log('Atualizou!');
-  }
+  
 
     render(){
         return(
             <div>
-                <h1>Meu projeto {this.state.Hora}</h1>
+                <Membro nome="Visitante"/>
             </div>
         );
     }
-
 }
 
 export default App;
