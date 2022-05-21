4c114e69847dafdb14b570d2cd47fcabb9651d4f
Diego Leandro <diegoleandro2002@hotmail.com>
2022-05-21 07:44:58 -0300

    Props and components pt.2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 
-const Bemvindo = (props) => {
-    return(
-        <div>
-            <h2>Bem vindo(a), {props.nome}</h2>
-            <h3>Tenho {props.idade} anos</h3>
-        </div>
-    );
+const Equipe = (props) => {
+  return(
+    <div>
+      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
+      <Social ig={props.insta}/>
+    </div>
+  );
 }
+
+const Sobre = (props) => {
+  return(
+    <div>
+      <h2>Olá, sou o(a) {props.nome}</h2>
+      <h3>Cargo: {props.cargo}</h3>
+      <h3>idade: {props.idade}</h3>
+      
+      
+    </div>
+  );
+}
+
+const Social = (props) => {
+  return(
+    <div>
+      <a>Github </a>
+      <a href={props.ig}>Instagram </a>
+      <a>Facebook </a>
+      <hr/>
+    </div>
+  );
+}
+
 
 function App(){
   return(
     <div>
-      Olá Mundo!
-      <Bemvindo nome="Diego" idade="20"/>
-      <Bemvindo nome="Leandro" idade="21"/>
-      <h1>Diglean</h1>
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe nome="Diego" cargo="Programador" idade="20" insta="https://instagram.com/diglean"/>
+      <Equipe nome="Guilherme" cargo="QA" idade="21" insta="https://google.com"/>
+      <Equipe nome="José" cargo="Programador" idade="32" insta="https://google.com"/>
     </div>
   );
 }
