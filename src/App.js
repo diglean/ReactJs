9e61fbc0c6b00f49eedc63c95fb109ff5ab5e23f
Diego Leandro <diegoleandro2002@hotmail.com>
2022-05-21 08:45:11 -0300

    states

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 
-class Equipe extends Component{
-    render(){
-      return(
-          <div>
-            <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
-            <hr/>
-          </div>
-      );
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+          nome: 'Diego',
+          contador: 0
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
     }
-}
 
-class Sobre extends Component{
+    aumentar(){
+      let state = this.state;
+      state.contador += 1;
+      this.setState(state);
+    }
+
+    diminuir(){
+      let state = this.state;
+
+      if(state.contador === 0){
+        alert('Opa, chegou a zero!');
+        return;
+      }
+      state.contador -= 1;
+      this.setState(state);
+    }
+
+
   render(){
     return(
-        <div>
-        <h2>Olá, sou o(a) {this.props.nome} </h2>
-        <h3>Cargo: {this.props.cargo} </h3>
-        <h3>Idade: {this.props.idade} </h3>
-        </div>
+      <div>
+        <h1>Contador</h1>
+        {this.state.nome}
+          <h3> 
+            <button onClick={this.diminuir}>-</button>
+            {this.state.contador}
+            <button onClick={this.aumentar}>+</button> 
+            </h3>
+      </div>
     );
   }
-}
-
-function App(){
-  return(
-    <div>
-      <h1>Conheça nossa equipe:</h1>
-      <Equipe nome="Diego" cargo="Programador" idade="20"/>
-      <Equipe nome="Guilherme" cargo="QA" idade="21"/>
-    </div>
-  );
 }
 
 export default App;
