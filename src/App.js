815ac8b5091ad5f565308894816126c627d95ae7
diglean <diegoleandro2002@gmail.com>
2022-05-25 08:58:22 -0300

    Connecting

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { toHaveAccessibleDescription } from '@testing-library/jest-dom/dist/matchers';
+import React, { Component} from 'react';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+        email: 'teste@teste.com',
+        senha: '',
+        sexo: 'Masculino'
+    };
+    this.trocaEmail = this.trocaEmail.bind(this);
+    this.trocaSexo = this.trocaSexo.bind(this);
+  }
+
+  trocaEmail(e){
+    let valorDigitado = e.target.value;
+    this.setState({email: valorDigitado});
+  }
+
+  trocaSexo(e){
+    let valorDigitado = e.target.value;
+    this.setState({sexo: valorDigitado})
+  }
+
+    render(){
+        return(
+            <div>
+              <h2>Login</h2>
+                Email: 
+                <input type="email" name="email" value={this.state.email} 
+                        onChange={this.trocaEmail}/> <br/><br/>
+                Senha: 
+                <input type="password" name="password" value={this.state.senha}
+                        onChange={(e) => this.setState({senha: e.target.value})}/> <br/><br/>
+                Sexo:
+                <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo}>
+                    <option value="masculino">Masculino</option>
+                    <option value="feminino">Feminino</option>
+                </select>
+
+                        <div>
+                          <h3>{this.state.email}</h3>
+                          <h3>{this.state.senha}</h3>
+                          <h3>{this.state.sexo}</h3>
+                        </div>
+            </div>
+        );
+    }
+}
+
+export default App;
